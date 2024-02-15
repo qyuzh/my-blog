67666490412628cc9dcbe189403372e083a48ea6
qyuzh <qyuzhang@139.com>
2024-02-15 21:57:58 +0800

    chore: update script

--- deploy/deploy-linux.sh
+++ deploy/deploy-linux.sh
@@ -5,5 +5,5 @@ cd docs/.vitepress/dist
 zip -r ../../../qyuzh.zip *
 cd ../../..
 scp qyuzh.zip root@qyuzh.com:/usr/local/nginx/html
-ssh root@qyuzh.com "echo 'unzip...' && cd /usr/local/nginx/html && unzip -oq qyuzh.zip -x 'logo.jpg' '*.webp' && echo 'deploy success'"
+ssh root@qyuzh.com "echo 'unzip...' && cd /usr/local/nginx/html && unzip -oq qyuzh.zip -x 'logo.jpg' && echo 'deploy success'"
 echo "===Deployment All finished==="
