a271a3c85dae1221ee67e35c887c020f1fd3c3f7
qyuzh <qyuzhang@139.com>
2025-04-05 17:27:44 +0800

    chore: update deployment script paths for server entry

--- deploy/deploy-linux.sh
+++ deploy/deploy-linux.sh
@@ -4,6 +4,6 @@ pnpm build
 cd docs/.vitepress/dist
 zip -r ../../../qyuzh.zip *
 cd ../../..
-scp qyuzh.zip qyuzh@qyuzh.com:~/deployment/web_entry/data/blog
-ssh qyuzh@qyuzh.com "echo 'unzip...' && cd ~/deployment/web_entry/data/blog && unzip -oq qyuzh.zip -x 'logo.jpg' && echo 'deploy success'"
+scp qyuzh.zip qyuzh@qyuzh.com:~/deployment/server_entry/data/blog
+ssh qyuzh@qyuzh.com "echo 'unzip...' && cd ~/deployment/server_entry/data/blog && unzip -oq qyuzh.zip -x 'logo.jpg' && echo 'deploy success'"
 echo "===Deployment All finished==="
